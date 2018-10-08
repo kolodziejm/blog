3138e24bc74b8cd64eefdc739129b5ba7fd0795a
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-10-08 18:42:57 +0200

    Started to create categories page, finished styled Link component

---
+++ src/components/Link.js
@@ -0,0 +1,8 @@
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+export default styled(Link)`
+    font-size: 2.4rem;
+    text-decoration: none;
+    color: #FF006E;
+`;
---
+++ src/pages/categories.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Link, graphql } from 'gatsby'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+
+import Layout from '../components/layout'
+
+import Navigation from '../components/navigation/Navigation';
+import PageHeader from '../components/typography/PageHeader';
+import StyledLink from '../components/Link';
+
+
+const CategoriesContainer = styled.ul`
+  list-style: none;
+  text-align: center;
+`;
+
+const CategoriesItem = styled.li`
+  margin-bottom: 2.5rem;
+`;
+
+
+class Categories extends React.Component {
+
+  state = {
+    showMobileNav: false
+  }
+
+  switchMobileNav = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      showMobileNav: !this.state.showMobileNav
+    })
+  }
+
+
+  render() {
+
+    const siteTitle = this.props.data.site.siteMetadata.title;
+    const siteDescription = this.props.data.site.siteMetadata.description;
+    const posts = this.props.data.allMarkdownRemark.edges;
+
+    let categoriesArr = [];
+    const categories = posts.forEach(post => {
+      categoriesArr.push(post.node.frontmatter.category);
+      // return (
+      // <CategoriesItem key={post.node.id}>
+      //   <StyledLink to={`/${post.node.frontmatter.category.toLowerCase()}`}>{post.node.frontmatter.category}</StyledLink>
+      // </CategoriesItem>
+      // )
+    })
+
+    console.log(categoriesArr)
+
+    return (
+      <Layout location={this.props.location}>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[{ name: 'Index page of the blog', content: siteDescription }]}
+          title={siteTitle}
+        />
+        <Navigation categoriesActive="true" hamburgerClicked={this.switchMobileNav} showMobileNav={this.state.showMobileNav}/>
+        <PageHeader margin="9rem 0 3rem 0">Post categories</PageHeader>
+        <CategoriesContainer>
+          {/* {categories} */}
+        </CategoriesContainer>
+      </Layout>
+    )
+  }
+}
+
+export default Categories;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            category
+          }
+        }
+      }
+    }
+  }
+`
